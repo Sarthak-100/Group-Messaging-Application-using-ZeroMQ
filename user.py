c0127d869ae77356aba1d2f180e01e7e2870780c
Sarthak-100 <sarthak20576@iiitd.ac.in>
2024-02-16 16:58:17 +0000

    Minor Edits

--- user.py
+++ user.py
@@ -2,35 +2,36 @@ import zmq
 import uuid
 
 class User:
-    def __init__(self, name,message_server_ip,uuid):
+    def __init__(self, name,message_server_ip,uuid,ip):
         self.name = name
         self.context = zmq.Context()
         self.socket = self.context.socket(zmq.REQ)
         self.socket.connect(f"tcp://{message_server_ip}")
         self.uuid = uuid
+        self.ip = ip
 
     def get_group_list(self):
         self.socket.send_json({'request': 'GET_GROUP_LIST', 'uuid': self.uuid})
         response = self.socket.recv_json()
-        print("\n".join([f"{name} - {ip_address}" for name, ip_address in response.items()]))
+        print("\n".join([f"{name} - {self.ip}:{ip_address}" for name, ip_address in response.items()]))
 
     def join_group(self,grp_port):
         grp_socket = zmq.Context().socket(zmq.REQ)
-        grp_socket.connect(f"tcp://localhost:{grp_port}")
+        grp_socket.connect(f"tcp://{self.ip}:{grp_port}")
         grp_socket.send_json({'request': 'JOIN', 'uuid': self.uuid, 'name': self.name})
         response = grp_socket.recv_string()
         return response
 
     def leave_group(self,grp_port):
         grp_socket = zmq.Context().socket(zmq.REQ)
-        grp_socket.connect(f"tcp://localhost:{grp_port}")
+        grp_socket.connect(f"tcp://{self.ip}:{grp_port}")
         grp_socket.send_json({'request': 'LEAVE', 'uuid': self.uuid})
         response = grp_socket.recv_string()
         return response
 
     def get_messages(self, grp_port, timestamp=None):
         grp_socket = zmq.Context().socket(zmq.REQ)
-        grp_socket.connect(f"tcp://localhost:{grp_port}")
+        grp_socket.connect(f"tcp://{self.ip}:{grp_port}")
         grp_socket.send_json({'request': 'GET_MESSAGES', 'uuid': self.uuid, 'timestamp': timestamp})
         response = grp_socket.recv_json()
         
@@ -38,14 +39,14 @@ class User:
 
     def send_message(self,grp_port,message):
         grp_socket = zmq.Context().socket(zmq.REQ)
-        grp_socket.connect(f"tcp://localhost:{grp_port}")
+        grp_socket.connect(f"tcp://{self.ip}:{grp_port}")
         grp_socket.send_json({'request': 'SEND_MESSAGE', 'uuid': self.uuid, 'message': message})
         response = grp_socket.recv_string()
         return response
 
 if __name__ == "__main__":
     user_name = input("Enter user name: ")
-    user = User(user_name,"localhost:2000",str(uuid.uuid1()))
+    user = User(user_name,"34.131.224.250:2000",str(uuid.uuid1()),"34.131.236.36")
 
     while True:
         print("\nMenu:")
